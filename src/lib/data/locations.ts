349b714f05f2c80e4832c5a9a9740cc6a195037d
Ashok <ashokiitbombay@gmail.com>
2026-06-06 19:02:00 +0530

    nt-scan

---
+++ src/lib/data/locations.ts
@@ -0,0 +1,5 @@
+export const REGION_LOCATIONS: Record<string, string[]> = {
+  "navi-mumbai": ["vashi", "sanpada", "juinagar", "nerul", "seawoods", "cbd-belapur", "kharghar", "kamothe", "kalamboli", "panvel", "new-panvel", "taloja", "ghansoli", "kopar-khairane", "airoli", "turbhe"],
+  "south-mumbai": ["colaba", "cuffe-parade", "fort", "churchgate", "marine-lines", "nariman-point", "worli", "parel", "lower-parel", "mahalaxmi", "byculla", "dadar"],
+  "western-suburbs": ["bandra", "khar", "santacruz", "vile-parle", "andheri", "jogeshwari", "goregaon", "malad", "kandivali", "borivali", "dahisar"]
+};
